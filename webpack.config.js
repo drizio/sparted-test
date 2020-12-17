24c7dc4864dcaacd0f3979c986de3e0d15424c0a
drizio <driziob@gmail.com>
2020-12-17 10:44:14 +0100

    Changed webpack config: Added plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: true,
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
   module: {
     rules: [{
         test: /\.js$/,
@@ -13,6 +27,10 @@ module.exports = {
         use: {
             loader: 'babel-loader'
         }
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
     }]
   }
 };
